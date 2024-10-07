bb9ae6ee463a185e4266f081c40569612ef6456e
praveen.kushwaha <praveenkushwaha.tech@gmail.com>
2024-10-07 13:51:53 +0530

    #

---
+++ Math/Math1.java
@@ -0,0 +1,26 @@
+public class Math1 {
+    static void decimalToAnyBase(int decimalNum, int base) {
+        int resNum = 0;
+        int power = 0;
+        while (decimalNum>0){
+            int rem = decimalNum%base;
+            decimalNum /= base;
+            resNum += rem * power(10,power);
+            power++;
+        }
+        System.out.println("Result is "+ resNum);
+    }
+
+    static double power(int num, int power){
+        double res = 1;
+        for(int i = 0; i< power;i++){
+            res = res*num;
+        }
+        System.out.println(res);
+        return res;
+    }
+    public static void main(String[] args) {
+        decimalToAnyBase(8,2);
+    }
+}
+
